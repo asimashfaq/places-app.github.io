18f75e554d88ee710dfba56c4835c0b013745be1
asimashfaq <asimizb4@gmail.com>
2019-11-09 19:41:25 +0500

    Add the markers on the map

--- src/modules/places/Places.tsx
+++ src/modules/places/Places.tsx
@@ -20,17 +20,32 @@ const gprops = {
 };
 
 const Marker = (props: any) => {
-  let photoItem =  props && props.data.photo && props.data.photo.response.photos && props.data.photo.response.photos.items[0];
-  return <div className="SuperAwesomePin">
-    <div>
-      {props.data && props.data.item.venue && props.data.item.venue.location.formattedAddress}
-    </div>
-    <div>
-      {photoItem && 
-       <img src={`${photoItem.prefix}${photoItem.width}x${photoItem.height}${photoItem.suffix}`}/>
-      } 
-    </div>
-  </div>;
+  let photoItem =
+    props &&
+    props.data.photo &&
+    props.data.photo.response.photos &&
+    props.data.photo.response.photos.items[0];
+    return (
+      <>
+          <div className="map-pin-wrapper text-xxs bg-white p-2 shadow-inner shadow-2xl rounded-lg" style={{minHeight: '50px'}}>
+              {photoItem &&
+              <div className="pins-image bg-cover rounded-lg"
+                   style={{backgroundImage: `url(${photoItem.prefix}${photoItem.width}x${photoItem.height}${photoItem.suffix})`}}>
+              </div>
+              }
+              <p className="mt-3 text-sm">
+                  {props.data.item.venue.location.formattedAddress.map((item,index) => {
+                      return (
+                          <p className={`${index === 0 ? "font-bold text-base mb-1" : ""}`}>{item}</p>
+                      )
+                  })}
+              </p>
+              <div className="h-8 w-8 bg-cover pt-1 absolute bottom-0 pin"
+                   style={{backgroundImage: `url(../../src/images/map-pin.png)`, paddingLeft: '4.5px'}}>
+              </div>
+          </div>
+      </>
+  )
 };
 
 const PlacesList: React.FC = () => {
@@ -48,7 +63,7 @@ const PlacesList: React.FC = () => {
   const dispatch = useDispatch();
 
   const { data, photo, isLoading, error } = useSelector(
-    ({ dataList, venuePic }: { dataList: IPlacesListState, venuePic: any }) => {
+    ({ dataList, venuePic }: { dataList: IPlacesListState; venuePic: any }) => {
       return {
         isLoading: dataList.isLoading,
         data: dataList.data,
@@ -81,7 +96,7 @@ const PlacesList: React.FC = () => {
     if (data.response.groups[0].items.length > 0) {
       result = data.response.groups[0].items.map((item: Item) => {
         markers.push({
-          data: {item, photo: photo.photos[item.venue.id]},
+          data: { item, photo: photo.photos[item.venue.id] },
           Lat: item.venue.location.lat,
           Lng: item.venue.location.lng
         });
@@ -91,7 +106,7 @@ const PlacesList: React.FC = () => {
   }
 
   return (
-    <div className="flex flex-col flex-1 lg:flex-row bg-secondary">
+    <div className="flex flex-col flex-1 lg:flex-row bg-secondary map-wrapper">
       <div className="w-full lg:w-2/3 h-map">
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyCGfO7rXQBR5j7EKJBafS3soCPpLhqW-J0" }}
@@ -104,7 +119,7 @@ const PlacesList: React.FC = () => {
           })}
         </GoogleMapReact>
       </div>
-      <div className="overflow-hidden w-full lg:w-1/3 bg-white relative">
+      <div className="overflow-auto w-full lg:w-1/3 bg-white relative">
         <div className="w-full move-right bg-white">{result}</div>
         <div
           className={`absolute top-0 left-0 absolutely-center hide-loader ${
