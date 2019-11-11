83076c61a930e34e2fd922541cec4fa9ed802ef2
asimashfaq <asimizb4@gmail.com>
2019-11-11 13:18:44 +0500

    Updates

---
+++ precache-manifest.5ff525b939b9f87fb5a6b7464dbb69e3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f442c8adb0d4171d2a43b4e7f76a123e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97fe9590f8c58dc14f76",
+    "url": "/static/js/2.79afb5ea.chunk.js"
+  },
+  {
+    "revision": "7b24bbb53f594724747d",
+    "url": "/static/js/main.6f2c6ace.chunk.js"
+  },
+  {
+    "revision": "74b02e83762065a8177f",
+    "url": "/static/js/runtime-main.dcf5906d.js"
+  }
+]);
